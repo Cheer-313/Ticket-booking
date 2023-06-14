ec83008228df9ffed00d0760b11dc1cda7690960
Mach Tin <machtrungtin2x@gmail.com>
2023-06-14 10:34:32 +0700

    card image fill

--- FE/components/card.vue
+++ FE/components/card.vue
@@ -4,9 +4,7 @@ defineProps(['artist'])
 <template>
     <div class="items-center rounded-xl shadow bg-gray-900 border-gray-700">
         <a href="#">
-            <img class=" rounded-lg sm:rounded-l-lg object-contain w-full h-48"
-                :src="artist['galerry_url']"
-                alt="">
+            <img class=" rounded-lg sm:rounded-l-lg object-fill w-full h-48" :src="artist['galerry_url']" alt="">
         </a>
         <div class="p-4">
             <h3 class="text-xl font-bold tracking-tight text-white">
