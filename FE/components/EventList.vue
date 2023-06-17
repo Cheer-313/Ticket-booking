1bdeff9dc4800f3ec58d92ee685114095c689aee
Mach Tin <machtrungtin2x@gmail.com>
2023-06-17 19:02:56 +0700

    Event filter page

--- FE/components/EventList.vue
+++ FE/components/EventList.vue
@@ -2,7 +2,7 @@
     <aside aria-label="Related articles" class="py-8 lg:py-24  bg-gray-800">
         <div class="px-4 mx-auto max-w-screen-xl">
             <h2 class="mb-8 text-2xl font-bold  text-white">Sự kiện khác</h2>
-            <div class="grid gap-12 sm:grid-cols-2 lg:grid-cols-3">
+            <div class="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
                 <article class="max-w-xs text-white border rounded-lg border-gray-500">
                     <a href="#">
                         <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png"
@@ -20,20 +20,20 @@
                                 </strong>
                             </div>
 
+                            <font-awesome-icon :icon="['fas', 'user-secret']" />
                             <p>
-                                <font-awesome-icon :icon="['fasr', 'calendar-days']" style="color:#2DC275;" />
                                 29.06.2023
                             </p>
                         </div>
                         <div class="flex justify-between py-2">
                             <div>
-                                <font-awesome-icon :icon="['fas', 'table-cells']" class="pr-1" />
+                                <font-awesome-icon icon="table-cells" />
                                 <a href="#" class=" inline-flex items-center underline-offset-4 hover:underline">
                                     Concert
                                 </a>
                             </div>
                             <p class="border rounded-lg p-1">
-                                <font-awesome-icon :icon="['fasr', 'location-dot']" />
+                                <font-awesome-icon :icon="['fas', 'location-dot']" />
                                 HO CHI MINH
                             </p>
                         </div>
