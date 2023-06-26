0e931eeed063576d0f8ef7b443c48bd3ddcefea9
nhhung <nhh3103@gmail.com>
2023-06-27 00:38:49 +0700

    20230627: fill data to view

--- FE/components/IntroduceEventInfo.vue
+++ FE/components/IntroduceEventInfo.vue
@@ -8,7 +8,7 @@ const { event } = toRefs(props);
             <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue format-invert text-gray-200">
                 <h1 class="mb-4 text-3xl font-extrabold leading-tightlg:mb-6 lg:text-4xl border-b-2 p-4">Introduce</h1>
                 <h2 class="mb-8 text-xl font-bold leading-tightlg:mb-6 lg:text-3xl text-center">{{ event.event_name }}</h2>
-                <p class="mb-4 text-lg" v-html="event.event_description.replace(/(?:\r\n|\r|\n)/g, '<br />')"></p>
+                <p v-if="event.event_description != null" class="mb-4 text-lg" v-html="event.event_description?.replace(/(?:\r\n|\r|\n)/g, '<br />')"></p>
             </article>
         </div>
     </section>
