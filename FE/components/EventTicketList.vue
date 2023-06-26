0dc0e4084bd946ba5f2f9b19e7b545ca0c94ae6a
nhhung <nhh3103@gmail.com>
2023-06-27 00:28:32 +0700

    20230627: fill data to view

--- FE/components/EventTicketList.vue
+++ FE/components/EventTicketList.vue
@@ -8,8 +8,7 @@ const {ticketSlot} = toRefs(props);
             <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue format-invert text-gray-200">
                 <h1 class="mb-4 text-3xl font-extrabold leading-tightlg:mb-6 lg:text-4xl border-b-2 p-4">Ticket Information</h1>
                 <div v-for="ticket in ticketSlot">
-                    <div
-                        class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 hover:bg-gray-800">
+                    <div class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 hover:bg-gray-800">
                         <h3>{{ ticket.seat_name }}</h3>
                         <div class="text-center">
                             <p class="font-bold">{{ new Intl.NumberFormat({ style: 'currency', currency: 'VND' }).format(ticket.price) }} VND</p>
--- FE/components/IntroduceEventInfo.vue
+++ FE/components/IntroduceEventInfo.vue
@@ -6,9 +6,9 @@ const { event } = toRefs(props);
     <section class="pb-8 pt-8  bg-gray-800">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue format-invert text-gray-200">
-                <h1 class="mb-4 text-3xl font-extrabold leading-tightlg:mb-6 lg:text-4xl border-b-2 p-4">Giới thiệu</h1>
+                <h1 class="mb-4 text-3xl font-extrabold leading-tightlg:mb-6 lg:text-4xl border-b-2 p-4">Introduce</h1>
                 <h2 class="mb-8 text-xl font-bold leading-tightlg:mb-6 lg:text-3xl text-center">{{ event.event_name }}</h2>
-                <p class="mb-4 text-lg">{{ event.event_description }}</p>
+                <p class="mb-4 text-lg" v-html="event.event_description.replace(/(?:\r\n|\r|\n)/g, '<br />')"></p>
             </article>
         </div>
     </section>
