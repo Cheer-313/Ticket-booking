ab50bf938b9fe190eed296ccbacb0f46908a8274
nhhung <nhh3103@gmail.com>
2023-06-27 00:03:29 +0700

    20230627: 20230625: fill data to view

--- FE/components/EventTicketList.vue
+++ FE/components/EventTicketList.vue
@@ -1,34 +1,20 @@
+<script setup>
+const props = defineProps(['ticketSlot']);
+const {ticketSlot} = toRefs(props);
+</script>
 <template>
     <section class=" pt-8 lg:pb-8 bg-gray-800">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue format-invert text-gray-200">
-                <h1 class="mb-4 text-3xl font-extrabold leading-tightlg:mb-6 lg:text-4xl border-b-2 p-4">Thông tin vé</h1>
-                <div>
+                <h1 class="mb-4 text-3xl font-extrabold leading-tightlg:mb-6 lg:text-4xl border-b-2 p-4">Ticket Information</h1>
+                <div v-for="ticket in ticketSlot">
                     <div
                         class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 hover:bg-gray-800">
-                        <h3>Hạng A</h3>
+                        <h3>{{ ticket.seat_name }}</h3>
                         <div class="text-center">
-                            <p class="font-bold">350.000 VND</p>
-                            <p class="border border-red-600 text-red-600 px-2 hidden">hết vé</p>
-                            <p class="border border-gray-200 px-2">Vé chưa mở bán</p>
-                        </div>
-                    </div>
-                    <div
-                        class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 hover:bg-gray-800">
-                        <h3>Hạng A</h3>
-                        <div class="text-center">
-                            <p class="font-bold">350.000 VND</p>
-                            <p class="border border-red-600 text-red-600 px-2">hết vé</p>
-                            <p class="border border-gray-200 px-2 hidden">Vé chưa mở bán</p>
-                        </div>
-                    </div>
-                    <div
-                        class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 hover:bg-gray-800">
-                        <h3>Hạng A</h3>
-                        <div class="text-center">
-                            <p class="font-bold">350.000 VND</p>
-                            <p class="border border-red-600 text-red-600 px-2 hidden">hết vé</p>
-                            <p class="border border-gray-200 px-2 hidden">Vé chưa mở bán</p>
+                            <p class="font-bold">{{ new Intl.NumberFormat({ style: 'currency', currency: 'VND' }).format(ticket.price) }} VND</p>
+                            <p v-if="ticket.total_slot - ticket.slot_booked < 1" class="border border-red-600 text-red-600 px-2 hidden">SOLD OUT</p>
+                            <!-- <p class="border border-gray-200 px-2">Vé chưa mở bán</p> -->
                         </div>
                     </div>
                 </div>
--- FE/pages/detail/event/[id].vue
+++ FE/pages/detail/event/[id].vue
@@ -3,6 +3,7 @@ const route = useRoute();
 const config = useRuntimeConfig();
 const eventId = ref();
 const eventDetail = ref([]);
+const ticketSlot = ref([]);
 
 eventId.value = route.params.id;
 // Get data artist
@@ -13,13 +14,22 @@ const { data:event, error } = await useFetch(config.public.MS1_API_URL+"/api/det
     },
 });
 eventDetail.value = event.value.event_detail;
+
+// Get data slot of ticket
+const { data:slots, error2 } = await useFetch(config.public.MS2_API_URL+"/api/ticket/slot/", {
+    method: 'GET',
+    query: {
+        event_id: eventId.value, 
+    },
+});
+ticketSlot.value = slots.value.ticket_slot;
 </script>
 
 <template>
     <div>
         <EventInfo :event="eventDetail[0]"/>
         <IntroduceEventInfo :event="eventDetail[0]"/>
-        <EventTicketList />
+        <EventTicketList :ticket-slot="ticketSlot"/>
         <!-- <EventList /> -->
     </div>
 </template>
