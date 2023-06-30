a85a5ebeddab43aac89533a2fb6b7c5c4b7ab322
Mach Tin <machtrungtin2x@gmail.com>
2023-06-30 17:02:24 +0700

    add confirm buy ticket page

---
+++ FE/pages/ConfirmBuyTicket.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <EventInfo />
+        <BuyTicketConfirmUserInfo />
+    </div>
+</template>
+
+<style>
+    button.bg-red-600, section.bg-gray-900 {
+        display: none;
+    }
+</style>
