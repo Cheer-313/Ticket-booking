24cea2c60cc60288103810c230115ea9878cfb0d
Mach Tin <machtrungtin2x@gmail.com>
2023-08-21 23:58:43 +0700

    update order history

--- FE/components/Profile/DetailOrder.vue
+++ FE/components/Profile/DetailOrder.vue
@@ -1,18 +1,105 @@
 <template>
-    <div class="max-w-4xl mx-auto" id="accordion-collapse" data-accordion="collapse">
-        <h2 id="accordion-collapse-heading-1">
-            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 bg-gray-700" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-            <span>What is Flowbite?</span>
-            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-            </svg>
-            </button>
-        </h2>
-        <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-            <div class="p-5 border border-gray-700 bg-gray-800">
-                <p class="mb-2 text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
-                <p class="text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+    <div class="mx-5">
+        <div class="max-w-4xl mx-auto pb-4" id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-1">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 bg-gray-700" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                <span>D&E WORLD TOUR FANCON - [DElight PARTY] IN HO CHI MINH</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+                <div class="p-5 border border-gray-700 bg-gray-800 flex justify-between text-gray-400">
+                    <div class="w-3/5">
+                        <p>Nhà thi đấu Phú Thọ - 1 Lữ Gia, Phường 15, Quận 11, Thành phố Hồ Chí Minh</p>
+                        <p>Saturday, 2 September 2023, 6:00 PM+0700</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Loại vé: <span id="">VIP</span></p>
+                            <p>Số lượng vé: <span id="">1</span></p>
+                            <p>Tổng tiền: <span id="">1.500.000 VNĐ</span></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="max-w-4xl mx-auto pb-4" id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-2">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 bg-gray-700" data-accordion-target="#accordion-collapse-body-2" aria-expanded="true" aria-controls="accordion-collapse-body-2">
+                <span>D&E WORLD TOUR FANCON - [DElight PARTY] IN HO CHI MINH</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+                <div class="p-5 border border-gray-700 bg-gray-800 flex justify-between text-gray-400">
+                    <div class="w-3/5">
+                        <p>Nhà thi đấu Phú Thọ - 1 Lữ Gia, Phường 15, Quận 11, Thành phố Hồ Chí Minh</p>
+                        <p>Saturday, 2 September 2023, 6:00 PM+0700</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Loại vé: <span id="">VIP</span></p>
+                            <p>Số lượng vé: <span id="">1</span></p>
+                            <p>Tổng tiền: <span id="">1.500.000 VNĐ</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-4xl mx-auto pb-4" id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-3">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 bg-gray-700" data-accordion-target="#accordion-collapse-body-3" aria-expanded="true" aria-controls="accordion-collapse-body-3">
+                <span>D&E WORLD TOUR FANCON - [DElight PARTY] IN HO CHI MINH</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+                <div class="p-5 border border-gray-700 bg-gray-800 flex justify-between text-gray-400">
+                    <div class="w-3/5">
+                        <p>Nhà thi đấu Phú Thọ - 1 Lữ Gia, Phường 15, Quận 11, Thành phố Hồ Chí Minh</p>
+                        <p>Saturday, 2 September 2023, 6:00 PM+0700</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Loại vé: <span id="">VIP</span></p>
+                            <p>Số lượng vé: <span id="">1</span></p>
+                            <p>Tổng tiền: <span id="">1.500.000 VNĐ</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-4xl mx-auto pb-4" id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-4">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border focus:ring-4 focus:ring-gray-800 border-gray-700 text-gray-400 bg-gray-700" data-accordion-target="#accordion-collapse-body-4" aria-expanded="true" aria-controls="accordion-collapse-body-4">
+                <span>D&E WORLD TOUR FANCON - [DElight PARTY] IN HO CHI MINH</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-4" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+                <div class="p-5 border border-gray-700 bg-gray-800 flex justify-between text-gray-400">
+                    <div class="w-3/5">
+                        <p>Nhà thi đấu Phú Thọ - 1 Lữ Gia, Phường 15, Quận 11, Thành phố Hồ Chí Minh</p>
+                        <p>Saturday, 2 September 2023, 6:00 PM+0700</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Loại vé: <span id="">VIP</span></p>
+                            <p>Số lượng vé: <span id="">1</span></p>
+                            <p>Tổng tiền: <span id="">1.500.000 VNĐ</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
